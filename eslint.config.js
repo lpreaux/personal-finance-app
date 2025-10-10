4998b38cab635f231b483c1d8ab7c73ce4dd78ba
Lucas Préaux <contact@lucaspreaux.fr>
2025-10-10 12:18:46 +0200

    switch from next lint to eslint since next lint is deprecated

--- eslint.config.js
+++ eslint.config.js
@@ -5,18 +5,16 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-export default tseslint.config(
+const config = [
   {
-    ignores: [".next"],
+    ignores: [".next", "convex/_generated", "node_modules"],
   },
   ...compat.extends("next/core-web-vitals"),
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
     files: ["**/*.ts", "**/*.tsx"],
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
     rules: {
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
@@ -45,4 +43,6 @@ export default tseslint.config(
       },
     },
   },
-);
+];
+
+export default config;
